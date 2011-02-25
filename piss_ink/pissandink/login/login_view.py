10e3aabf01c2fbca578b17b3a406f79f50c7c1f9
root <root@ausfahrt.databahn.info>
2011-02-25 12:29:02 -0500

    user logins working

--- piss_ink/pissandink/login/login_view.py
+++ piss_ink/pissandink/login/login_view.py
@@ -13,30 +13,33 @@ import MySQLdb
 def login(request):
 	#connection to our database
 	conn = MySQLdb.connect("localhost","piss","pisswithink","piss" )
-	cursor = conn.cursor (MySQLdb.cursors.DictCursor)
-	cursor1 = conn.cursor (MySQLdb.cursors.DictCursor)
+	cursor = conn.cursor()
+	cursor1 = conn.cursor()
 	#gets the user name and password from the user for checking
 	user_name = request.GET.get('user_name')
 	passwd = request.GET.get('password')
 	#new_user=request.session['user_name']
 	#preforms a query to find the correct password
-	cursor.execute("""SELECT user_name,password FROM user WHERE user_name = '%s'""" % (user_name))
-	results = cursor.fetchallDict()
+	cursor.execute("""SELECT password FROM user WHERE user_name = '%s'""" % (user_name))
+	results = cursor.fetchone()
 	#gets password from the returned query and sets it to correct_password
-	correct_password = results[0]
+	correct_password = results
 	
 	cursor1.execute("""SELECT PASSWORD('%s') FROM dual;""" % (passwd))
-	user_results = cursor1.fetchall()
-	entered_password = user_results[0]			
+	entered_results = cursor1.fetchone()
+	entered_password = entered_results			
 	conn.close()
-	return render_to_response('login.html', {'results':results, 'entered_password':entered_password})#,'user_password': user_password})#, context_instance=RequestContext(request))
+	if user_name == None:
+		return HttpResponseRedirect('databahn.info/')
 	if correct_password == entered_password:
-		return HttpResponseRedirect('/home/')
+		return HttpResponseRedirect('home/')
+	return render_to_response('login.html', {'correct_password':correct_password, 'entered_password':entered_password})#,'user_password': user_password})#, context_instance=RequestContext(request))
+				
 
 @csrf_exempt
 def adduser(request):
 	db = MySQLdb.connect("localhost","piss","pisswithink","piss" )
-	cursor = db.cursor (MySQLdb.cursors.DictCursor)
+	cursor = db.cursor()
 	user_name = request.POST.get('user_name')
 	first_name = request.POST.get('first_name')
 	last_name = request.POST.get('last_name')
@@ -44,12 +47,12 @@ def adduser(request):
 	password = request.POST.get('passwd')
 	password2 = request.POST.get('passwd2')
 	email = request.POST.get('email')
-	request.session['user_name']=user_name	
-	if password == password2:
+	if password != None and password == password2:
 		cursor.execute("""INSERT INTO piss.user(first_name,last_name,zip_code,password,user_name) VALUES('%s', '%s', '%s', PASSWORD('%s'), '%s');""" % (first_name, last_name, zip_code, password, user_name)) 
 		db.commit()		
 		cursor.execute("""INSERT INTO piss.ofUser(username, plainPassword, encryptedPassword, name, email) VALUES('%s','%s', PASSWORD('%s'), '%s', '%s');""" % (user_name, password, password, first_name, email))	
 		db.commit()
 		db.close()
+		return HttpResponseRedirect('databahn.info')
 	return render_to_response('adduser.html', {'user_name':user_name, 'first_name':first_name, 'last_name':last_name, 'zip_code':zip_code, 'password1':password, 'password2':password2})# context_instance=RequestContext(request))
 	
