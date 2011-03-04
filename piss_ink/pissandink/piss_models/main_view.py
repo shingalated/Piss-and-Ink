77de9f1547bc193cafcd12208c5ce91cec456767
root <root@ausfahrt.databahn.info>
2011-03-04 11:16:59 -0500

    weather app working, config view started

--- piss_ink/pissandink/piss_models/main_view.py
+++ piss_ink/pissandink/piss_models/main_view.py
@@ -13,17 +13,27 @@ import MySQLdb
 
 @csrf_exempt
 def main_modules(request):
+	conn = MySQLdb.connect("localhost","piss","pisswithink","piss" )
+	cursor = conn.cursor()
 	all_events = Event.objects.all().order_by('-title')[:8]  #gets all of the objects in Events by title
 	#event_list_form = eventCheck(request.POST) #a model for the event list checkbox form
+	
 	username = request.session['username']
 	new_event = request.POST.get('event')  #gets the newly added event from the event form
-	google_results = pywapi.get_weather_from_google('05401')
-	weather = google_results['temp_c']
+	
+	#####gets the weather according to the user zip code###
+	cursor.execute("""SELECT zip_code FROM piss.user WHERE user_name = '%s'""" % (username))
+	zip_code = cursor.fetchone()
+	google_results = pywapi.get_weather_from_google('%s' % (zip_code))
+	city = google_results['forecast_information']['city']
+	temperature = google_results['current_conditions']['temp_f']
+	####end weather app###
+	
 	if new_event:
 		add_event = Event(title = new_event)
 		add_event.save()
 	#removal = request.POST.getlist('event_check')
-	return render_to_response('main_page.html',{ 'event_list': all_events, 'new_event': new_event, 'user':username, 'weather': weather}, context_instance=RequestContext(request))
+	return render_to_response('main_page.html',{'zip_code':zip_code, 'event_list': all_events, 'new_event': new_event, 'user':username, 'city': city, 'temperature':temperature}, context_instance=RequestContext(request))
 
 @csrf_exempt
 def EventDelete(request):
